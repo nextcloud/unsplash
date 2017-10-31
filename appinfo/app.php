e958f555f27029355738f4f3578e7a9c4bd76312
Lukas Reschke <lukas@statuscode.ch>
2017-10-31 10:55:41 +0100

    Proper CSP
    Signed-off-by: Lukas Reschke <lukas@statuscode.ch>

--- appinfo/app.php
+++ appinfo/app.php
@@ -3,8 +3,7 @@
 OCP\Util::addscript('unsplash', 'script');
 
 $manager = \OC::$server->getContentSecurityPolicyManager();
+$policy = new \OC\Security\CSP\ContentSecurityPolicy();
 $policy->addAllowedImageDomain('https://source.unsplash.com');
-$policy->addAllowedStyleDomain('\'self\'');
-$policy->addAllowedStyleDomain('\'unsafe-inline\'');
-$policy->addAllowedScriptDomain('\'self\'');
+$policy->addAllowedImageDomain('https://images.unsplash.com');
 $manager->addDefaultPolicy($policy);
