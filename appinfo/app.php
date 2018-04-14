cfc247ab99abf2ca8c0e3df3563f480334ea1ed6
Felix <felix.nuesse@t-online.de>
2018-04-14 09:47:54 +0200

    Removed htaccess dependency

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,6 +1,25 @@
 <?php
 
-OCP\Util::addStyle('unsplash', 'login');
+namespace OCA\Unsplash;
+
+use OC;
+use OCP\IConfig;
+
+
+$unsplashScript = get_included_files();
+$unsplashScript = $unsplashScript[0]; //gets the current filepath
+$unsplashScript=substr($unsplashScript, strlen($_SERVER['DOCUMENT_ROOT']), -9);
+
+
+\OCP\Util::addHeader(
+    'link',
+    [
+        'rel'  => "stylesheet",
+        'type' =>"text/css",
+        'href' => $unsplashScript."apps/unsplash/css/login.php",
+    ]
+);
+
 
 $manager = \OC::$server->getContentSecurityPolicyManager();
 $policy = new \OC\Security\CSP\ContentSecurityPolicy();
