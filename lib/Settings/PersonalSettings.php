c9f7673df826df35e7468a9afbe9cceacbc2eaa7
Felix Nüsse <felix.nuesse@t-online.de>
2022-10-06 18:48:08 +0200

    restore personal page with copyright attribution
    Signed-off-by: Felix Nüsse <felix.nuesse@t-online.de>

---
+++ lib/Settings/PersonalSettings.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * This file is part of the Unsplash App
+ * and licensed under the AGPL.
+ */
+
+namespace OCA\Unsplash\Settings;
+
+use OC_Defaults;
+use OCA\Unsplash\Services\SettingsService;
+use OCP\AppFramework\Http\TemplateResponse;
+use OCP\Settings\ISettings;
+
+/**
+ * Class PersonalSettingsController
+ *
+ * @package OCA\Unsplash\Controller\Settings
+ */
+class PersonalSettings implements ISettings {
+
+    /**
+     * @var SettingsService
+     */
+    protected $settings;
+
+    /**
+     * @var OC_Defaults
+     */
+    protected $theming;
+
+
+    /**
+     * AdminSection constructor.
+     *
+     * @param SettingsService $settings
+     * @param OC_Defaults     $theming
+     */
+    public function __construct(SettingsService $settings, OC_Defaults $theming) {
+        $this->settings     = $settings;
+        $this->theming      = $theming;
+    }
+
+    /**
+     * @NoAdminRequired
+     * @NoCSRFRequired
+     *
+     * @return TemplateResponse
+     */
+    public function getForm() {
+
+        $dashboard = $this->settings->getServerStyleDashboardEnabled();
+        $login = $this->settings->getServerStyleLoginEnabled();
+
+        if($this->settings->getImageProviderName() == "Nextcloud Image") {
+            $dashboard = false;
+            $login = false;
+        }
+
+        return new TemplateResponse('unsplash', 'settings/personal', [
+            'selectedProvider'=> str_replace(' ', '', $this->settings->getImageProviderName()),
+            'label'           => $this->theming->getEntity(),
+            'dashboard'         => $dashboard,
+            'login'      => $login,
+        ], '');
+    }
+
+    /**
+     * @return string
+     */
+    public function getSection() {
+        return "theming";
+    }
+
+    /**
+     * @return int
+     */
+    public function getPriority(): int {
+        return 75;
+    }
+}
---
+++ templates/settings/personal.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of the Unsplash App
+ * and licensed under the AGPL.
+ */
+
+
+if($_['dashboard'] || $_['login']):
+
+    script('unsplash', 'user');
+    style('unsplash', 'settings');
+
+    ?>
+
+<div class="section" id="unsplash-settings">
+    <h2>
+        <?php p($l->t('Random Background Images')); ?>
+    </h2>
+    <p class="settings-hint">
+        <?php p($l->t('%s is customized with random background images.', [$_['label']])); ?>
+        <?php if($_['dashboard']) {p($l->t('The images will be displayed as the background if you choose \'Default Image\' as your background.', [$_['label']]));} ?>
+        <br>
+        <br>
+        <?php print_unescaped($this->inc('partials/license'.$_['selectedProvider'])); ?>
+    </p>
+</div>
+
+<?php endif; ?>
